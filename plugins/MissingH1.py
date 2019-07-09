d362e1274fd622c622f75925b3b13ee7cf77e164
Ben Gosney <bengosney@googlemail.com>
2019-07-09 14:10:03 +0100

    Plugins for missing h1's and multipul h1's

---
+++ plugins/MissingH1.py
@@ -0,0 +1,17 @@
+class MissingH1:
+    def __init__(self, crawler):
+        self.missing_h1s = []
+        self.crawler = crawler
+
+    def get_results_headder(self):
+        return ['url']
+        
+    def get_results(self):
+        return self.missing_h1s
+        
+    def parse(self, html_soup, url=None):
+        h1s = html_soup.find_all('h1')
+
+        if len(h1s) == 0:
+            self.missing_h1s.append([url])
+            self.crawler.print("Found no H1 on {}".format(url))
---
+++ plugins/MultipleH1.py
@@ -0,0 +1,19 @@
+class MultipleH1:
+    def __init__(self, crawler):
+        self.multiple_h1s = []
+        self.crawler = crawler
+
+    def get_results_headder(self):
+        return ['url', 'h1s']
+        
+    def get_results(self):
+        return self.multiple_h1s
+        
+    def parse(self, html_soup, url=None):
+        h1s = html_soup.find_all('h1')
+
+        count = len(h1s)
+        if count > 1:
+            h1s = [h1.getText() for h1 in h1s].insert(0, url)
+            self.multiple_h1s.append(h1s)
+            self.crawler.print("Found {} H1s on {}".format(count, url))
