7e4f06a48bcee744994d08a28d8b31c63d331d96
Ben Gosney <bengosney@googlemail.com>
2019-07-11 17:22:52 +0100

    Don't let one bad result block to stop the pdf

--- plugins/ToPDF.py
+++ plugins/ToPDF.py
@@ -10,16 +10,19 @@ class ToPDF:
     def process_results(self, results):
         text = "#SEO Results"
         for block in results:
-            title = re.sub('(?<!^)([A-Z0-9]+)', ' \\1', block)
-            text = f"{text}\n\n##{title}\n"
-
-            cols = max([len(r) for r in results[block]])
-            table = ""
-            headdings = []
-            for i, row in enumerate(results[block]):
-                text = f"{text}\n| {' | '.join(row[:5])} | {'   |' * (cols - len(row))}"
-                if i == 0:
-                    text = f"{text}\n|{'---|' * cols}"
+            try:
+                title = re.sub('(?<!^)([A-Z0-9]+)', ' \\1', block)
+                text = f"{text}\n\n##{title}\n"
+            
+                cols = max([len(r) for r in results[block]])
+                table = ""
+                headdings = []
+                for i, row in enumerate(results[block]):
+                    text = f"{text}\n| {' | '.join(row[:5])} | {'   |' * (cols - len(row))}"
+                    if i == 0:
+                        text = f"{text}\n|{'---|' * cols}"
+            except:
+                pass
 
         html = markdown(text, output_format='html4', extensions=['tables'])
         report_path = os.path.join(self.crawler.results_base_path, f'report-for-{self.crawler.base_netloc}.pdf')
