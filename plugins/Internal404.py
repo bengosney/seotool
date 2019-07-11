a9e2d571c72a06fee9a199225ac1c3b86baf2eb8
Ben Gosney <bengosney@googlemail.com>
2019-07-11 17:20:14 +0100

    Plugin to find internal 404s

---
+++ plugins/Internal404.py
@@ -0,0 +1,40 @@
+import urllib.parse
+
+class Internal404:
+    def __init__(self, crawler):
+        self.crawler = crawler
+        self.links = {}
+        self.f404s = []
+
+    def get_results_header(self):
+        return ['url', 'pages']
+    
+    def get_results(self):
+        out = []
+        
+        for url in self.f404s:
+            out.append([url] + self.links[url])
+
+        return out            
+        
+    def parse(self, html_soup, url=None, status_code=None):
+        if url == None or status_code == None:
+            return
+        
+        if status_code == 404:
+            self.f404s.append(url)
+            self.crawler.printERR(f"404 found: {url}")
+        else:
+            links = html_soup.find_all('a')
+
+            for link in links:                
+                try:
+                    abs_url = urllib.parse.urljoin(self.crawler.base_url, link['href'])
+                except KeyError:
+                    continue
+
+                try:
+                    self.links[abs_url].append(url)
+                except KeyError:
+                    self.links.update({abs_url: [url]})
+        
