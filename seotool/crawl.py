16e6ace14f6b6216bc0ec8f4dad971229b455611
Ben Gosney <bengosney@googlemail.com>
2021-03-30 20:34:28 +0100

    Refactor for easier testing and typing

--- seotool/crawl.py
+++ seotool/crawl.py
@@ -6,6 +6,8 @@ import re
 import string
 import urllib.parse
 from collections import deque
+from functools import cached_property
+from typing import Callable, List
 
 # Third Party
 import click
@@ -13,10 +15,12 @@ import pkg_resources
 import urllib3
 from bs4 import BeautifulSoup
 from requests import head
-from requests.exceptions import TooManyRedirects
+from requests.exceptions import MissingSchema, TooManyRedirects
 
 # First Party
 from engines import EngineException
+from engines.dataModels import response
+from engines.engines import engine
 from processors import Processor
 from seotool.exceptions import SkipPage
 
@@ -24,19 +28,16 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 class Crawler:
-    _regex_whitespace = re.compile(r"\s+")
-    _regex_not_special = re.compile(f"[^{string.ascii_letters}{string.digits}_\\-. ]")
-
-    def __init__(self, url, plugins=[], verbose=True, verify=True, disabled=[], delay=0, engine="pyppeteer", plugin_options={}):
+    def __init__(self, url: str, plugins=[], verbose=True, verify=True, disabled=[], delay=0, engine="pyppeteer", plugin_options={}) -> None:
+        self.url = url
         self.verify = verify
-        self.base_url = head(url, verify=verify).url
         self.plugins = plugins if len(plugins) else None
         self.plugin_classes = []
         self.plugin_pre_classes = []
         self.plugin_post_classes = []
         self.visited = deque([])
-        self.urls = deque([self.base_url])
-        self.all_urls = [self.base_url]
+        self.urls = deque([])
+        self.all_urls = []
         self.resolve_cache = {}
         self.disabled = disabled
         self.delay = delay
@@ -46,37 +47,47 @@ class Crawler:
         self.verbose = verbose
 
         self._init_plugins(plugin_options)
-
-        if self.base_url != url:
-            self.print(f"\nBase URL {url} resolved to {self.base_url}\n", "yellow")
-
-        self.base_netloc = urllib.parse.urlparse(self.base_url).netloc
-        self.results_base_path = os.path.join(os.getcwd(), f"results-{self.base_netloc}")
-
         self.engine_instance = None
 
+    @cached_property
+    def base_url(self) -> str:
+        try:
+            return head(self.url, verify=self.verify).url
+        except MissingSchema:
+            self.url = f"https://{self.url}"
+        finally:
+            return head(self.url, verify=self.verify).url
+
+    @cached_property
+    def base_netloc(self) -> str:
+        return urllib.parse.urlparse(self.base_url).netloc
+
+    @cached_property
+    def results_base_path(self) -> str:
+        return os.path.join(os.getcwd(), f"results-{self.base_netloc}")
+
     @staticmethod
-    def get_plugin_list():
+    def get_plugin_list() -> List[str]:
         p = Processor(None)
         return p.plugin_names
 
     @staticmethod
-    def get_plugin_options():
+    def get_plugin_options() -> List[List[Callable]]:
         p = Processor(None)
         return p.get_options()
 
     @classmethod
-    def get_extra_options(cls):
+    def get_extra_options(cls) -> List[List[Callable]]:
         return cls.get_plugin_options() + [cls.get_engine_options()]
 
-    def skip_page(self):
+    def skip_page(self) -> None:
         raise SkipPage
 
-    def _init_plugins(self, plugin_options={}):
+    def _init_plugins(self, plugin_options={}) -> None:
         self.processor = Processor(self, self.plugins, self.disabled, plugin_options)
         self.print(f"Loaded plugins: {', '.join(self.processor.plugin_names)}")
 
-    def _add_links(self, html_soup):
+    def _add_links(self, html_soup) -> None:
         links = html_soup.find_all("a")
         for link in links:
             try:
@@ -94,14 +105,14 @@ class Crawler:
                 self.urls.append(abs_url)
                 self.all_urls.append(abs_url)
 
-    def print(self, text, color="white"):
+    def print(self, text, color="white") -> None:
         if self.verbose:
             click.secho(text, fg=color)
 
-    def printERR(self, text):
+    def printERR(self, text) -> None:
         self.print(text, "red")
 
-    def save_results(self):
+    def save_results(self) -> None:
         self.print(f"\nSaving results to {self.results_base_path}\n", "green")
 
         try:
@@ -112,7 +123,7 @@ class Crawler:
         results_store = self.processor.get_results_sets()
         self.processor.process_results_sets(results_store)
 
-    def get_output_name(self, name: str, extention: str, folder: str = ""):
+    def get_output_name(self, name: str, extention: str, folder: str = "") -> str:
         path = os.path.join(self.results_base_path, folder)
         try:
             os.makedirs(path)
@@ -121,13 +132,21 @@ class Crawler:
 
         return os.path.join(path, f"{self._clean_filename(name)}.{extention}")
 
-    @classmethod
-    def _clean_filename(cls, filename):
-        cleaned_filename = cls._regex_not_special.sub(" ", filename)
-        return cls._regex_whitespace.sub("-", cleaned_filename[:255]).lower()
+    @staticmethod
+    def _clean_filename(filename) -> str:
+        regex_not_special = re.compile(f"[^{string.ascii_letters}{string.digits}_\\-. ]")
+        regex_whitespace = re.compile(r"\s+")
 
-    async def crawl(self):
+        cleaned_filename = regex_not_special.sub(" ", filename)
+        return regex_whitespace.sub("-", cleaned_filename[:255]).lower().strip("-")
+
+    def reset_urls(self) -> None:
+        self.urls = deque([self.base_url])
+        self.all_urls = [self.base_url]
+
+    async def crawl(self) -> None:
         self._crawling = True
+        self.reset_urls
 
         engine = await self.getEngine()
         async with engine:
@@ -140,7 +159,7 @@ class Crawler:
         self.save_results()
 
     @staticmethod
-    def get_engine_options():
+    def get_engine_options() -> List[Callable]:
         options = []
         for entry_point in pkg_resources.iter_entry_points("seo_engines"):
             engine_cls = entry_point.load()
@@ -148,7 +167,7 @@ class Crawler:
 
         return options
 
-    async def getEngine(self):
+    async def getEngine(self) -> engine:
         if self.engine_instance is None:
             engine_cls = None
             for entry_point in pkg_resources.iter_entry_points("seo_engines"):
@@ -166,14 +185,14 @@ class Crawler:
 
         return self.engine_instance
 
-    async def getResponse(self, url):
+    async def getResponse(self, url) -> response:
         engine = await self.getEngine()
         try:
             return await engine.get(url, verify=self.verify)
         except Exception as ERR:
             raise EngineException() from ERR
 
-    def should_process(self, url, response):
+    def should_process(self, url, response) -> bool:
         self.resolve_cache.update({url: response.url})
         if url != response.url and response.url in self.visited:
             self.print(f"{url} resolves to {response.url} and has already been visited", "yellow")
@@ -189,7 +208,7 @@ class Crawler:
 
         return True
 
-    async def _crawl(self):
+    async def _crawl(self) -> None:
         while self._crawling:
             if self.delay:
                 await asyncio.sleep(self.delay)
