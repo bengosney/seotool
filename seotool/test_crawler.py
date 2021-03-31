5bfa2ccdad41d9da0de137bba6781dd2b1a19fe7
Ben Gosney <bengosney@googlemail.com>
2021-03-31 08:55:21 +0100

    Test that the crawler actualy crawls pages

--- seotool/test_crawler.py
+++ seotool/test_crawler.py
@@ -1,5 +1,6 @@
 # Third Party
 import pytest
+from pytest_httpserver import HTTPServer
 
 # First Party
 from seotool.crawl import Crawler
@@ -22,3 +23,15 @@ def test_skip_page():
     c = Crawler("localhost")
     with pytest.raises(SkipPage):
         c.skip_page()
+
+
+def test_can_crawl(httpserver: HTTPServer):
+    httpserver.expect_request("/page2").respond_with_data("<h1>page2</h1>", content_type="text/html")
+    page2_url = httpserver.url_for("/page2")
+    httpserver.expect_request("/").respond_with_data(f'<a href="{page2_url}">page 2</a>', content_type="text/html")
+
+    crawler = Crawler(httpserver.url_for("/"), verbose=False)
+    crawler.asyncio_crawl(save=False)
+
+    assert len(crawler.all_urls) == 2
+    assert page2_url in crawler.all_urls
