c27a3fd742661068bf8f51e03beedb945225be04
Ben Gosney <bengosney@googlemail.com>
2021-04-02 15:40:57 +0100

    Ignore tel: and mailto:

--- processors/plugins/tests/test_IgnoreURISchemes.py
+++ processors/plugins/tests/test_IgnoreURISchemes.py
@@ -6,21 +6,27 @@ from processors.plugins.IgnoreURISchemes import IgnoreURISchemes
 
 
 def test_ignore_uri_schemes():
+    valid = [
+        '<a href="/link">link</a>',
+        '<a href="http://www.example.com/">example</a>',
+        '<a href="https://www.example.com/">example</a>',
+    ]
+    invalid = [
+        '<a href="tel://00000000000">Phone</a>',
+        '<a href="tel:00000000000">Phone</a>',
+        '<a href="mailto:example@example.com">email</a>',
+    ]
+
     html = BeautifulSoup(
-        """
-<a href="/link">link</a>
-<a href="http://www.example.com/">example</a>
-<a href="https://www.example.com/">example</a>
-<a href="tel://00000000000">Phone</a>
-    """,
+        "".join(valid + invalid),
         "html.parser",
     )
 
     links = html.find_all("a")
-    assert len(links) == 4
+    assert len(links) == len(valid + invalid)
 
     plugin = IgnoreURISchemes()
     plugin.process_html(html, None)
 
     links = html.find_all("a")
-    assert len(links) == 3
+    assert len(links) == len(valid)
