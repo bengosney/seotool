af854b6e1bd46cdbaa2af9ef826db2ddc5ed3b73
Ben Gosney <bengosney@googlemail.com>
2021-04-02 12:25:18 +0100

    Test missing H1

---
+++ processors/plugins/tests/test_MissingH1.py
@@ -0,0 +1,38 @@
+# Third Party
+from bs4 import BeautifulSoup
+
+# First Party
+from processors.plugins.MissingH1 import MissingH1
+from seotool.crawl import Crawler
+
+c = Crawler("")
+
+
+def test_missing_h1():
+    html = BeautifulSoup(
+        """
+<p>Page Content</p>
+    """,
+        "html.parser",
+    )
+
+    plugin = MissingH1(c)
+    plugin.process(html=html, url="/")
+    res = plugin.get_results_set()
+
+    assert res.data == [{"url": "/"}]
+
+
+def test_not_missing_h1():
+    html = BeautifulSoup(
+        """
+<h1>Page Content</h1>
+    """,
+        "html.parser",
+    )
+
+    plugin = MissingH1(c)
+    plugin.process(html=html, url="/")
+    res = plugin.get_results_set()
+
+    assert res.data == []
