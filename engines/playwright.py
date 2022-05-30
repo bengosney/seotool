7491375c20226726e599b4b2782cc19baee89c40
Ben Gosney <bengosney@googlemail.com>
2022-05-30 15:28:36 +0100

    Add screenshot option to playwright

--- engines/playwright.py
+++ engines/playwright.py
@@ -1,12 +1,10 @@
-# Third Party
-
-# Third Party
+import click
 from playwright.async_api import async_playwright
 
 # First Party
 from engines import engine, response
 
-
+from icecream import ic
 class playwright(engine):
     playwright = None
     browser = None
@@ -38,4 +36,13 @@ class playwright(engine):
         page = await self.browser.new_page()
         result = await page.goto(url)
 
+        if self.screenshot:
+            title = await page.title()
+            path = self.crawler.get_output_name(title, "png", "screenshots")
+            await page.screenshot(path=path, full_page=True)
+
         return response(headers=result.headers, status_code=result.status, url=result.url, body=await page.content())
+
+    @staticmethod
+    def get_options():
+        return [click.option("--screenshot", is_flag=True, help="Take screen shots of every page (playwrite only)")]
