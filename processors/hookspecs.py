e101ec099ef835898fb9125030f690fa9edbebe0
Ben Gosney <bengosney@googlemail.com>
2022-06-01 08:35:37 +0100

    Fix hooktype

--- processors/hookspecs.py
+++ processors/hookspecs.py
@@ -15,31 +15,39 @@ class processor:
     @hookspec_processor
     def process_html(self, html: BeautifulSoup, url: str, response: dict, status_code: str) -> None:
         """Pre-process the html before any checks are run."""
+        ...
 
     @hookspec_processor
     def process(self, html: BeautifulSoup, url: str, response: dict, status_code: str) -> None:
         """Process the html."""
+        ...
 
     @hookspec_processor
     def get_results_set(self) -> ResultSet | None:
         """Get a results object."""
+        ...
 
     @hookspec_processor
     def process_output(self, resultsSets: list[ResultSet]):
         """Process the data into a format."""
+        ...
 
     @hookspec_processor
     def get_options(self):
         """Get any cli arguments."""
+        ...
 
     @hookspec_processor
     def should_process(self, url: str, response: response) -> bool:
         """Do we want to process this url?"""
+        ...
 
     @hookspec_processor(firstresult=True)
     def log(self, line, style):
         """Output any log messages."""
+        ...
 
     @hookspec_processor(firstresult=True)
     def log_error(self, line):
         """Output any error messages."""
+        ...
