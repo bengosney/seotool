eba3324c4e8d53feca2bfcb25f3b90720284485e
Ben Gosney <bengosney@googlemail.com>
2023-03-13 09:06:04 +0000

    Fix the timeout tests

--- engines/tests/test_playwright.py
+++ engines/tests/test_playwright.py
@@ -9,6 +9,7 @@ from werkzeug.wrappers import Request, Response
 # First Party
 from engines.playwright import playwright
 from seotool.crawl import Crawler
+from seotool.exceptions import Timeout
 
 
 @pytest.mark.asyncio
@@ -22,5 +23,23 @@ async def test_timeout(httpserver: HTTPServer):
 
     engine = playwright(crawler, timeout=1)
 
-    async with engine:
-        await engine.get(httpserver.url_for("/"))
+    with pytest.raises(Timeout):
+        async with engine:
+            await engine.get(httpserver.url_for("/"))
+
+
+@pytest.mark.asyncio
+async def test_no_timeout(httpserver: HTTPServer):
+    def quick_response(_: Request) -> Response:
+        return Response()
+
+    httpserver.expect_request("/").respond_with_handler(quick_response)
+    crawler = Crawler(httpserver.url_for("/"), verbose=False)
+
+    engine = playwright(crawler)
+
+    try:
+        async with engine:
+            await engine.get(httpserver.url_for("/"))
+    except Timeout as e:
+        assert False, f"Engine timed out {e}"
